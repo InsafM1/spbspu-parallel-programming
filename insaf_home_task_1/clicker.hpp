148221c1970c57c0bbf7280b4f248d7a32fb6fd6
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-09-17 20:51:41 +0300

    Add the class Clicker

---
+++ insaf_home_task_1/clicker.hpp
@@ -0,0 +1,19 @@
+#include <chrono>
+
+class Clicker
+{
+public:
+  Clicker():
+    start_(std::chrono::high_resolution_clock::now())
+  {}
+  double millisec() const
+  {
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::milliseconds;
+    auto t = high_resolution_clock::now();
+    return duration_cast< milliseconds >(t - start_).count();
+  }
+private:
+  std::chrono::time_point< std::chrono::steady_clock > start_;
+};
