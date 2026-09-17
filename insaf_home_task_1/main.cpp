e34d5ca2cb9912e86ed4fe02bac64accf757aff6
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-09-17 22:22:10 +0300

    Done home task

--- insaf_home_task_1/main.cpp
+++ insaf_home_task_1/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <future>
 #include <vector>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include "clicker.hpp"
 
-size_t summation(
-  const std::vector< int >& vector,
+using data_t = std::vector< unsigned long long >;
+using value_t = data_t::value_type;
+
+value_t summation(
+  const std::vector< unsigned long long >& vector,
   size_t start,
   size_t end
 )
 {
-  size_t sum = 0;
+  value_t sum = 0;
   for (size_t i = start; i < end; ++i)
   {
     sum += vector[i];
@@ -16,18 +23,18 @@ size_t summation(
   return sum;
 }
 
-size_t parallel_summation(
-  const std::vector< int >& vector,
+value_t parallel_summation(
+  const std::vector< unsigned long long >& vector,
   size_t number_of_process
 )
 {
   size_t s = vector.size();
   size_t length = (s + number_of_process - 1) / number_of_process;
-  std::vector< std::future< size_t > > futures;
+  std::vector< std::future< value_t > > futures;
   for (size_t i = 0; i < number_of_process; ++i)
   {
-    size_t start = number_of_process * length;
-    size_t end = std::min(start + length, number_of_process);
+    size_t start = i * length;
+    size_t end = std::min(start + length, s);
     futures.push_back(
       std::async(
         std::launch::async,
@@ -38,7 +45,8 @@ size_t parallel_summation(
       )
     );
   }
-  size_t total = 0;
+
+  value_t total = 0;
   for (size_t i = 0; i < number_of_process; ++i)
   {
     total += futures[i].get();
@@ -46,7 +54,50 @@ size_t parallel_summation(
   return total;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc != 2)
+  {
+    std::cerr << "Exactly one argument is expected\n";
+    return 1;
+  }
+
+  size_t number_of_process = 0;
+  try
+  {
+    size_t pos = 0;
+    number_of_process = std::stoi(argv[1], &pos);
+    if (pos != std::strlen(argv[1]))
+    {
+      std::cerr << "Invalid argument: incorrectly recorded numbers\n";
+      return 1;
+    }
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << "Invalid argument: " << e.what() << '\n';
+    return 1;
+  }
+
+  if (number_of_process == 0)
+  {
+    std::cerr << "Argument must be > 0\n";
+    return 0;
+  }
+
+  constexpr size_t size{200'000'000};
+  double init{0}, total{0};
+  value_t sum{0};
+  data_t values(size, 1);
+  double result{0};
+  for (size_t i = 0; i < 5; ++i)
+  {
+    Clicker clicker;
+    sum += parallel_summation(values, number_of_process);
+    result += clicker.millisec();
+    sum = 0;
+  }
+
+  std::cout << number_of_process << ": " << result / 5.0 << '\n';
   return 0;
 }
